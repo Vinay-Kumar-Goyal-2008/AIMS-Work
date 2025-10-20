d98855d2d2396b3e803ad5abc847bc33112038ac
Vinay-Kumar-Goyal-2008 <vinaykumargoyal2008@gmail.com>
2025-10-20 14:54:45 +0530

    Some changes in the assignment done

--- Assignment1/main2.py
+++ Assignment1/main2.py
@@ -11,10 +11,25 @@ df=pd.DataFrame({'id':[i for i in range(1,11)],
 
 
 #Filling all nan values of the dataframe
-
-# filling marks column with mean
-
-df['Marks']=np.where(df['Marks'].isnull(),df['Marks'].mean(),df['Grade'])
+print('How do you want to fill the null values in the marks column')
+print('''1. Mode
+      2. Median
+      3.Mean
+      4. Drop the rows''')
+try:
+    ch=int(input('Enter the choice:- '))
+except Exception as e:
+    print(e)
+if (ch==1):
+    df['Marks']=np.where(df['Marks'].isnull(),df['Marks'].mode()[0],df['Grade'])
+elif (ch==2):
+    df['Marks']=np.where(df['Marks'].isnull(),df['Marks'].median(),df['Grade'])
+elif ch==3:
+    df['Marks']=np.where(df['Marks'].isnull(),df['Marks'].mean(),df['Grade'])
+elif ch==4:
+    df=df.dropna(subset=['Marks'])
+else:
+    print("Wrong choice!")
 #filling grade column with mode
 df['Grade']=np.where(df['Grade'].isnull(),df['Grade'].mode()[0],df['Grade'])
-
+print('filled the nan values in the grade column with mode')
