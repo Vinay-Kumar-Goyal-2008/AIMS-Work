80ff64499fecca7c8f205ede36d1f681c300ab2c
Vinay-Kumar-Goyal-2008 <vinaykumargoyal2008@gmail.com>
2025-11-04 16:26:59 +0530

    new one hot encoding code added

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,18 +1,21 @@
 import pandas as pd
 import numpy as np
 import random
-
-#Data Frame
-
-df=pd.DataFrame({'id':[i for i in range(1,11)],
-                 'Marks':[23,24,24,np.nan,12,24,np.nan,34,24,57],
-                 'Grade':['B','C','A',np.nan,'A','C','D',np.nan,'A','F'],
-                 'Gender':[random.choice(['M','F']) for i in range (1,11)]})
-
-#One Hot Encoding on the Gender column
-
-df['Male']=np.where(df['Gender']=='M',1,0)
-df['Female']=np.where(df['Gender']=='F',1,0)
-df=df.drop(columns=['Gender'])
-
-print(df)
+try:
+  def onehotencoder(df,col):
+    uniqueelements=df[col].unique()
+    for i in uniqueelements:
+      df[i]=np.where(df[col]==i,1,0)
+    df=df.drop(columns=[col])
+    return df
+  
+  
+  if __name__=="__name__":
+    
+    df=pd.DataFrame({'id':[i for i in range(1,11)],
+                     'Marks':[23,24,24,np.nan,12,24,np.nan,34,24,57],
+                     'Grade':['B','C','A',np.nan,'A','C','D',np.nan,'A','F'],
+                     'Gender':[random.choice(['M','F']) for i in range (1,11)]})
+    print(onehotencoder(df,'Gender'))
+except Exception as e:
+  print(e)
