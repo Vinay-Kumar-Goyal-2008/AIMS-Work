fb9198440d51bf4dc52af9f3399a15fe5d70e41b
Vinay-Kumar-Goyal-2008 <vinaykumargoyal2008@gmail.com>
2025-11-04 16:37:23 +0530

    new ordinal encoding script added

--- Assignment1/main1.py
+++ Assignment1/main1.py
@@ -1,21 +1,20 @@
 import pandas as pd
 import numpy as np
 import random
-
-#Data Frame
-
-df=pd.DataFrame({'id':[i for i in range(1,11)],
-                 'Marks':[23,24,24,np.nan,12,24,np.nan,34,24,57],
-                 'Grade':['B','C','A',np.nan,'A','C','D',np.nan,'A','F'],
-                 'Gender':[random.choice(['M','F']) for i in range (1,11)]})
-
-#Ordinal Encoding on the Grade column along with filling nan values in the required column
-
-#filling nan values in the grade column
-
-df['Grade']=np.where(df['Grade'].isnull(),df['Grade'].mode()[0],df['Marks'])
-
-#Encoding the grade column
-
-df['Grade']=np.where(df['Grade']=='F',0,np.where(df['Grade']=='D',1,np.where(df['Grade']=='C',2,np.where(df['Grade']=='B',3,np.where(df['Grade']=='A',4,5)))))
-print(df)
+try:
+  def ordinalencoding(df,col,ranklist):
+    indexlist=[i for i in range(0,len(ranklist))]
+    zippedlist=list(zip(ranklist,indexlist))
+    for i in zippedlist:
+      np.where(df[col]==i[0],i[1],df[col])
+    return df
+  
+  if __name__="__main__":
+    df=pd.DataFrame({'id':[i for i in range(1,11)],
+                     'Marks':[23,24,24,np.nan,12,24,np.nan,34,24,57],
+                     'Grade':['B','C','A',np.nan,'A','C','D',np.nan,'A','F'],
+                     'Gender':[random.choice(['M','F']) for i in range (1,11)]})
+    
+    print(ordinalencoding(df,'Grade',['A','B','C','D','E','F']))
+except Exception as e:
+  print(e)
